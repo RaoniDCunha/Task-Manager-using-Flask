99134843cb0512e97824d0b32a5cdb312dfba903
unknown <raonidutracunha@ymail.com>
2026-06-14 18:19:28 -0300

    chore: Adicionado teste de ferramenta para capturar dados da aplicacao (teste 4)

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,16 +1,19 @@
 import os
 
+from prometheus_flask_exporter import PrometheusMetrics
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from prometheus_flask_exporter import PrometheusMetrics
+
 
 
 app = Flask(__name__)
 
+
 metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Métricas da aplicação Task Manager', version='1.0.0')
+
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '45cf93c4d41348cd9980674ade9a7356')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///site.db')
